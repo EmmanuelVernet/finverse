b2db4937cec2ea4dc4c8cec61588d9f8233b326e
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2026-04-06 21:26:45 +0200

    adding seed data logic to seed file...

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+   has_many :aml_alerts_reviewed, class_name: "AmlAlert", foreign_key: "reviewed_by_id", dependent: :nullify
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,17 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-# (compliance_officer/support_agent/compliance_manager/admin)
+# Clean DB tables
+puts "cleaning DB tables"
+User.delete_all
+puts "Cleaned DB table data"
+
 # Your own personal account
+puts "Creating users..."
 User.create!(
   first_name: "Emmanuel",
   last_name: "Vernet",
-  email: "#{first_name.downcase}.#{last_name.downcase}@finversebank.com",
+  email: "emmanuel.vernet@finversebank.com",
   role: "admin"
 )
 
@@ -28,3 +33,67 @@ User.create!(
     role: [ "compliance_officer", "support_agent", "compliance_manager", "admin" ].sample
   )
 end
+
+puts "Created #{User.count} users"
+
+# Customers
+puts "Creating customers..."
+5.times do |i|
+  Customer.create!(
+    company_name: "Company #{i}",
+    legal_name: "Company #{i} Ltd",
+    registration_number: "REG#{1000 + i}",
+    customer_type: ["SME", "Enterprise"].sample,
+    email: "customer#{i}@example.com",
+    onboarding_status: :approved,
+    risk_score: rand(0..100),
+    country: ["FR", "DE", "US"].sample
+  )
+end
+puts "Created #{Customer.count} customers"
+
+# BankAccounts
+puts "Creating Bank accounts..."
+Customer.all.each do |customer|
+  2.times do
+    BankAccount.create!(
+      customer: customer,
+      account_number: "FR#{rand(10**22..10**23 - 1)}",
+      currency: ["EUR", "USD"].sample,
+      status: :active,
+      current_balance: rand(1000..100_000).to_f
+    )
+  end
+end
+puts "Created #{Customer.count} customers"
+
+# Transactions
+puts "Creating transactions..."
+BankAccount.all.each do |account|
+  5.times do
+    Transaction.create!(
+      bank_account: account,
+      amount: rand(10..10_000).to_f,
+      currency: account.currency,
+      transaction_type: ["debit", "credit"].sample,
+      status: ["pending", "cleared", "failed"].sample
+    )
+  end
+end
+puts "Created #{Transaction.count} transactions"
+
+# AMLAlerts
+puts "Creating AML Alerts..."
+Transaction.all.sample(5).each do |txn|
+  AmlAlert.create!(
+    customer: txn.bank_account.customer,
+    bank_account: txn.bank_account,
+    triggering_transaction: txn,
+    reviewed_by: User.first, # or nil
+    alert_type: ["large_transfer", "unusual_pattern", "sanctioned_entity"].sample,
+    severity: ["low", "medium", "high", "critical"].sample,
+    status: ["open", "under_review", "escalated", "closed"].sample,
+    notes: "Generated for seeding"
+  )
+end
+puts "Created #{AmlAlert.count} AML Alerts"
