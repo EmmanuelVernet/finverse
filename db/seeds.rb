14ad5de8176b683dcde6ccd4db3750ae7579cdfb
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2026-04-06 21:10:25 +0200

    added faker gem & user creation in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,24 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# (compliance_officer/support_agent/compliance_manager/admin)
+# Your own personal account
+User.create!(
+  first_name: "Emmanuel",
+  last_name: "Vernet",
+  email: "#{first_name.downcase}.#{last_name.downcase}@finversebank.com",
+  role: "admin"
+)
+
+4.times do |i|
+  first_name = Faker::Name.first_name
+  last_name  = Faker::Name.last_name
+
+  User.create!(
+    first_name: first_name,
+    last_name: last_name,
+    email: "#{first_name.downcase}.#{last_name.downcase}@finversebank.com",
+    role: [ "compliance_officer", "support_agent", "compliance_manager", "admin" ].sample
+  )
+end
