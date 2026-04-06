df653d4d329ca5cf18d8beb7366d50887c76249f
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2026-04-06 19:49:26 +0200

    added validations to bank acct model

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -1,3 +1,5 @@
 class BankAccount < ApplicationRecord
   belongs_to :customer
+  enum :status, { active: 0, frozen: 1, closed: 2 }, prefix: true
+  validates :account_number, :currency, presence: true
 end
