9e43d1d3ee6aabaa8216b917bc7c9904092503ab
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2026-04-07 22:01:24 +0200

    fix: database/yml conflict with create_agent.rb

--- lib/forest_admin_rails/create_agent.rb
+++ lib/forest_admin_rails/create_agent.rb
@@ -1,9 +1,17 @@
 module ForestAdminRails
   class CreateAgent
-    def self.setup!
-      database_configuration = Rails.configuration.database_configuration
-      datasource = ForestAdminDatasourceActiveRecord::Datasource.new(database_configuration[Rails.env])
+    # def self.setup!
+    #   database_configuration = Rails.configuration.database_configuration
+    #   datasource = ForestAdminDatasourceActiveRecord::Datasource.new(database_configuration[Rails.env])
 
+    #   @create_agent = ForestAdminAgent::Builder::AgentFactory.instance.add_datasource(datasource)
+    #   customize
+    #   @create_agent.build
+    # end
+    def self.setup!
+      datasource = ForestAdminDatasourceActiveRecord::Datasource.new(
+        ActiveRecord::Base.connection_db_config.configuration_hash
+      )
       @create_agent = ForestAdminAgent::Builder::AgentFactory.instance.add_datasource(datasource)
       customize
       @create_agent.build
