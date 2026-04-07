4a910dfbd68563a4fa1808cc6b80fc99512ddca5
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2026-04-07 14:05:39 +0200

    ran forest agent install command

---
+++ config/initializers/forest_admin_rails.rb
@@ -0,0 +1,4 @@
+ForestAdminRails.configure do |config|
+  config.auth_secret = '4f580f25423c5507438d9ca3b61fbdb9e0780a73'
+  config.env_secret = '76087e73569bf631fe0e3f1da292292c93a1be139f22c99107ce18a4a5ad642a'
+end
